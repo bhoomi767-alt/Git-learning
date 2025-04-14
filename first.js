136c81bc35a758fa93599e51e4256c21c961f22a
Bhoomi Dubey <bhoomidubey333@gmail.com>
2025-04-15 00:02:04 +0530

    payment updated after footer

--- first.js
+++ first.js
@@ -4,5 +4,10 @@ console.log(button);
 const login = "login page added"
 console.log(login);
 
+<<<<<<< HEAD
 const footer = "footer added in our website"
 console.log(footer);
+=======
+const payment = "integrated the payment gate way"
+console.log(payment);
+>>>>>>> f22a7c7 (payment added)
