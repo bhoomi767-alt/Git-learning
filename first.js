be6a2be68a01f4f20ad7965c25cfc621f691f69c
Bhoomi Dubey <bhoomidubey333@gmail.com>
2025-04-15 14:40:58 +0530

    latest changes

--- first.js
+++ first.js
@@ -4,16 +4,11 @@ console.log(button);
 const login = "login page added"
 console.log(login);
 
-<<<<<<< HEAD
 const footer = "footer added in our website"
 console.log(footer);
-=======
 const payment = "integrated the payment gate way"
 console.log(payment);
-<<<<<<< HEAD
->>>>>>> f22a7c7 (payment added)
-=======
-
 const upi = "integrated the upi"
 console.log(upi);
->>>>>>> f60bc5f (upi added)
+
+console.log("Latest update");
